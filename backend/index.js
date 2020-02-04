cf4e7c77e3ee2b34b1de6f131ce02b0068e1d288
DawidIz <41062760+DawidIz@users.noreply.github.com>
2020-02-04 14:33:45 +0100

    Update index.js

--- backend/index.js
+++ backend/index.js
@@ -1,14 +1,15 @@
 const app = require('express')()
 const http = require('http').createServer(app)
 const io = require('socket.io')(http)
+const gpio = require('onoff').Gpio
 
 const DATA = [
-    {id : 20, text : 'light', value : 0},
-    {id : 21, text : 'outlet', value : 0},
+    {id : 20, text : 'Light', value : 0 , gpio : new gpio(20,'out')},
+    {id : 21, text : 'Socket', value : 0, gpio : new gpio(21,'out')},
 ]
 
 io.on('connection', socket => {
-    console.log(`new connection socket id: ${socket.id}`)
+    console.log(`connection id: ${socket.id} ip: ${socket.handshake.address}`)
 
     socket.on('init',() => {
         socket.emit('init', DATA)
@@ -18,13 +19,24 @@ io.on('connection', socket => {
         const item = DATA.find(item => item.id === id)
         
         if(item){
-            console.log('found')
-            item.value = item.value ^ 1
-            io.emit('toggle value',{id, value : item.value})
+            const pin = item.gpio
+
+            pin.read((err,value) => {
+                if(err) throw err
+
+                item.value = value ^ 1
+
+                pin.write(item.value, err => {
+                    if(err) throw err
+
+                    io.emit('toggle value',{id, value : item.value})
+                })
+            })
         }
         else console.log('not found')
     })
 
+    socket.on('disconnect',() => console.log(`disconnected id: ${socket.id}`))
 })
 
 http.listen(4000,() => console.log('Listening'))
